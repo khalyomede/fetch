95c9c4a3f720603b8c6561873bbeaec89ea1d9dc
khalyomede <khalyomede@gmail.com>
2018-03-01 23:48:27 +0100

    added cache feature to help obtain data faster

--- src/Fetch.php
+++ src/Fetch.php
@@ -14,6 +14,8 @@
 	namespace Khalyomede;
 
 	use InvalidArgumentException;
+	use RuntimeException;
+	use UnexpectedValueException;
 
 	class Fetch {
 		/**
@@ -22,69 +24,134 @@
 		public $folder_path;
 
 		/**
+		 * @var string
+		 */
+		public $cache_folder_path;
+
+		/**
+		 * @var bool
+		 */
+		public $cache_disabled;
+
+		/**
 		 * Set the default folder to fetch the data from
 		 *
 		 * @throws InvalidArgumentException
 		 */
 		public function __construct(string $folder_path) {
 			if( file_exists($folder_path) === false ) {
-				throw new InvalidArgumentException(sprintf('Fetch::using expect parameter 1 to be an existing folder, but no folder found at location "%s"', $folder_path));
+				throw new InvalidArgumentException(sprintf('Fetch::using expects parameter 1 to be an existing folder, but no folder found at location "%s"', $folder_path));
 			}
 
 			if( is_dir($folder_path) === false ) {
 				$folder_name = basename($folder_path);
 
-				throw new InvalidArgumentException(sprintf('Fetch::using expect parameter 1 to be a folder, but "%s" seems to not be a folder', $folder_name));
+				throw new InvalidArgumentException(sprintf('Fetch::using expects parameter 1 to be a folder, but "%s" seems to not be a folder', $folder_name));
 			}
 
 			$this->folder_path = rtrim($folder_path, '/');
+			$this->cache_disabled = true;
+			$this->cache_path_folder = '';
+		}
+
+		/**
+		 * @throws InvalidArgumentException
+		 */
+		public function usingCache(string $cache_folder_path): Fetch {
+			if( file_exists($cache_folder_path) === false ) {
+				throw new InvalidArgumentException(sprintf('Fetch::usingCache expects parameter 1 to be an existing folder, "%s" not found', $cache_folder_path));
+			}
+
+			if( is_dir($cache_folder_path) === false ) {
+				throw new InvalidArgumentException(sprintf('Fetch::usingCache expects parameter 1 to be a folder, but "%s" is not', $cache_folder_path));
+			}
+
+			$this->cache_folder_path = rtrim($cache_folder_path, '/');
+			$this->cache_disabled = false;
+
+			return $this;
+		}
+
+		public function disableCache(): Fetch {
+			$this->cache_disabled = true;
+
+			return $this;
+		}
+
+		public function enableCache(): Fetch {
+			$this->cache_disabled = false;
+
+			return $this;
 		}
 
 		/**
 		 * @return mixed
 		 * @throws InvalidArgumentException
+		 * @throws UnexpectedValueException
+		 * @throws RuntimeException
 		 */
 		public function from(string $path) {
-			$value = false;
+			$cached_path = $this->cache_folder_path . "/$path.php";
 
-			$parts = explode('.', $path);
-
-			$file_found = false;
-			$partial_path = $this->folder_path;
-
-			foreach( $parts as $index => $part ) {
-				if( strlen(trim($part)) === 0 ) {
-					throw new InvalidArgumentException(sprintf('Fetch::from encountered an empty key for the key n.%s in the path "%s"', ($index + 1), $path));
+			if( $this->cache_disabled === false && file_exists($cached_path) ) {
+				if( strlen($this->cache_folder_path) === 0 ) {
+					throw new UnexpectedValueException('Fetch::from in cache mode expects you to set the cache folder path using Fetch::usingCache');
 				}
 
-				if( $file_found === true ) {
-					if( isset($value[$part]) === false ) {
-						throw new InvalidArgumentException(sprintf('Fetch::from failed to resolve path "%s" because of the key n.%s "%s"', $path, ($index + 1), $part));
-					}
+				if( is_readable($cached_path) === false ) {
+					throw new RuntimeException(sprintf('Fetch::from faced a non-readable file error for cached file "%s" (maybe it is open in another program ?)', $cached_path));
+				}
 
-					$value = $value[$part];
-				}
-				else if( is_file("$partial_path/$part.php") === true ) {
-					$file_found = true;
-					$value = require("$partial_path/$part.php");
-
-					if( is_array($value) === false ) {
-						throw new InvalidArgumentException(sprintf('Fetch::from expect parameter 1 to target a file return an array, "%s" return type found instead in the file located at "%s"', gettype($value), "$partial_path/$part.php"));
-					}
-				}
-				else if( is_dir("$partial_path/$part") === true ) {
-					if( is_readable("$partial_path/$part") === false ) {
-						throw new InvalidArgumentException(sprintf('Fetch::from could not open file at location "%s" (maybe it is already opened by another program?)', "$partial_path/$part"));
-					}
-
-					$partial_path .= "/$part";
-				}
-				else {
-					throw new InvalidArgumentException(sprintf('Fetch::from could not resolve path "%s"', $path));
-				}
+				return require($cached_path);
 			}
+			else {
+				$value = false;
 
-			return $value;
+				$parts = explode('.', $path);
+
+				$file_found = false;
+				$partial_path = $this->folder_path;
+
+				foreach( $parts as $index => $part ) {
+					if( strlen(trim($part)) === 0 ) {
+						throw new InvalidArgumentException(sprintf('Fetch::from encountered an empty key for the key n.%s in the path "%s"', ($index + 1), $path));
+					}
+
+					if( $file_found === true ) {
+						if( isset($value[$part]) === false ) {
+							throw new InvalidArgumentException(sprintf('Fetch::from failed to resolve path "%s" because of the key n.%s "%s"', $path, ($index + 1), $part));
+						}
+
+						$value = $value[$part];
+					}
+					else if( is_file("$partial_path/$part.php") === true ) {
+						$file_found = true;
+						$value = require("$partial_path/$part.php");
+
+						if( is_array($value) === false ) {
+							throw new InvalidArgumentException(sprintf('Fetch::from expects parameter 1 to target a file return an array, "%s" return type found instead in the file located at "%s"', gettype($value), "$partial_path/$part.php"));
+						}
+					}
+					else if( is_dir("$partial_path/$part") === true ) {
+						if( is_readable("$partial_path/$part") === false ) {
+							throw new InvalidArgumentException(sprintf('Fetch::from could not open file at location "%s" (maybe it is already opened by another program?)', "$partial_path/$part"));
+						}
+
+						$partial_path .= "/$part";
+					}
+					else {
+						throw new InvalidArgumentException(sprintf('Fetch::from could not resolve path "%s"', $path));
+					}
+				}
+
+				if( $this->cache_disabled === false ) {
+					$php_code = var_export($value, true);
+
+					file_put_contents($this->cache_folder_path . "/$path.php", "<?php return $php_code; ?>");
+				}
+
+				return $value;
+			}
 		}
 	}
 ?>
